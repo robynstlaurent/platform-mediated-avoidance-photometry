ea568856093954a3b7273ad9818f9f8fe3b3c0db
robynstlaurent <70971036+robynstlaurent@users.noreply.github.com>
2025-11-21 14:59:54 -0800

    Update getfNames_phot.m
    Matching names to provided example data

--- getfNames_phot.m
+++ getfNames_phot.m
@@ -4,8 +4,8 @@
 fnames = { 
 % % Genotype = FoxP2-cre x SAPAP WT
 % 
-'RS08162021a_09272021' % Shock Day 1
-'RS08162021a_10082021' % Shock Day 10
+'RS12192023c_02012023' % Shock Day 1
+'RS12192023c_02122023' % Shock Day 10
 
 % you can keep adding to this list or pull from a csv of filenames
     };
