937215ca5389d14fa347c7df4a35184535046481
robynstlaurent <70971036+robynstlaurent@users.noreply.github.com>
2025-11-21 14:45:30 -0800

    Create getfNames_phot.m

---
+++ getfNames_phot.m
@@ -0,0 +1,11 @@
+% getfNames_phot.m
+% List of file names
+
+fnames = { 
+% % Genotype = FoxP2-cre x SAPAP WT
+% 
+'RS08162021a_09272021' % Shock Day 1
+'RS08162021a_10082021' % Shock Day 10
+
+% you can keep adding to this list or pull from a csv of filenames
+    };
